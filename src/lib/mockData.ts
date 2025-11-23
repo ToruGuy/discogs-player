77e86ac07071cfb1d693371520a6cfe19d6fcd53
ToruGuy <browner342@gmail.com>
2025-11-23 21:51:34 +0100

    Update mock data import to use unified mocked_data_albums.json for album data

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -1,4 +1,4 @@
 import type { Album } from '../types';
-import mockDataRaw from '../../mocked_data_20_albums.json';
+import mockDataRaw from '../../mocked_data_albums.json';
 
 export const MOCK_ALBUMS: Album[] = mockDataRaw as Album[];
